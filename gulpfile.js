f8c573c018430513b7ecea9d72ff57f85213f458
Mikael <mantism@seas.upenn.edu>
2017-12-22 05:23:01 -0500

    [Prerelease] Bumped version number

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,88 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+var conventionalChangelog = require('gulp-conventional-changelog');
+var minify = require('gulp-minify');
+var git = require('gulp-git');
+var fs = require('fs');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('changelog', () => {
+	return gulp.src('CHANGELOG.md', {
+		buffer: false
+	})
+	.pipe(conventionalChangelog({
+		preset: 'angular'
+	}))
+	.pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', () => {
+	return gulp.src('.')
+	  .pipe(git.add())
+	  .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('push-changes', (callback) => {
+	git.push('origin', 'master', callback);
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('./src/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('minify-html', () => {
+  return gulp.src('./src/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('minify-js', () => {
+  return gulp.src('./src/script.js')
+    .pipe(minify({
+      ext: {
+				src:'-debug.js',
+        min:'.js'
+      }
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('move-res', () => {
+  return gulp.src('./src/res/**/*')
+    .pipe(gulp.dest('./res'));
+});
+
+gulp.task('minify', (callback) => {
+  runSequence(
+    'move-res',
+    'minify-css',
+    'minify-html',
+    'minify-js',
+    (error) => {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('Minify finished successfully');
+      }
+      callback(error);
+    });
+});
+
+gulp.task('deploy', (callback) => {
+  runSequence(
+    'minify',
+    'changelog',
+    'commit-changes',
+		'push-changes',
+    (error) => {
+      if (error) {
+			  console.log(error.message);
+			} else {
+				console.log('Successfully deployed');
+			}
+			callback(error);
+		});
 });
---
+++ src/script.js
@@ -0,0 +1,95 @@
+var isInView = function (element) {
+  var elementTop = element.offset().top;
+  var elementBot = elementTop + element.outerHeight();
+  var screenBot = $(window).scrollTop() + $(window).height();
+  return (screenBot > elementTop) && (screenBot <  elementBot);
+
+}
+
+var buttons = ['#about-button', '#work-button', '#code-button', '#tricking-button'];
+
+var toggleActive = function (element) {
+  for (var i = 0; i < buttons.length; i++) {
+    if (element === buttons[i]) {
+      $(buttons[i]).addClass('active');
+    } else {
+      $(buttons[i]).removeClass('active');
+    }
+  }
+}
+
+$(window).scroll(function() {
+  if (isInView($('#about'))) {
+    toggleActive('#about-button');
+    $('header').css('border-bottom', 'solid #40cde4  1px');
+    $('header').css('border-top', 'solid #40cde4  1px');
+  }
+
+  if (isInView($('#work'))) {
+    toggleActive('#work-button');
+    $('header').css('border-bottom', 'solid #f49445  1px');
+    $('header').css('border-top', 'solid #f49445  1px');
+  }
+
+  if (isInView($('#code'))) {
+    toggleActive('#code-button');
+    $('header').css('border-bottom', 'solid #5aa814  1px');
+    $('header').css('border-top', 'solid #5aa814 1px');
+  }
+
+  if (isInView($('#tricking'))) {
+    toggleActive('#tricking-button');
+    $('header').css('border-bottom', 'solid #d42700 1px');
+    $('header').css('border-top', 'solid #d42700 1px');
+  }
+});
+
+$(document).ready( function() {
+  $('#about-button').click(function() {
+    toggleActive('#about-button');
+    $('header').css('border-bottom', 'solid #40cde4  1px');
+    $('header').css('border-top', 'solid #40cde4  1px');
+  });
+
+  $('#work-button').click(function() {
+    toggleActive('#work-button');
+    $('header').css('border-bottom', 'solid #f49445  1px');
+    $('header').css('border-top', 'solid #f49445  1px');
+  });
+
+  $('#code-button').click(function() {
+    toggleActive('#code-button');
+    $('header').css('border-bottom', 'solid #5aa814  1px');
+    $('header').css('border-top', 'solid #5aa814 1px');
+  });
+
+  $('#tricking-button').click(function() {
+    toggleActive('#tricking-button');
+    $('header').css('border-bottom', 'solid #d42700 1px');
+    $('header').css('border-top', 'solid #d42700 1px');
+  });
+
+  $("a[href^='#']").click(function(e) {
+    if (this.hash !== ' ') {
+      e.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 50
+      }, 800, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  $(".topnav").click(function(e) {
+    var x = document.getElementById("links");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        console.log("sup");
+    } else {
+        x.className = "topnav";
+    }
+  })
+});
